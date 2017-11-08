d6de54a2711eec28638c26b821c8ca2d9d262570
Unknown <dhea23mar@gmail.com>
2017-11-08 10:31:48 +0800

    update homepage

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # PINTU GTD Website
 
 To initialize:
-1. You should have npm and yarn installed.
+1. You should have npm, node and yarn installed.
 2. I am using Atom as the text editor
 ```sh
 $ git clone https://github.com/dheamariesta/gtd.git
 $ cd gtd
 $ yarn install
 $ atom .
+$ yarn start
 ```
 
 
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './Home.css';
 import {Link} from 'react-router-dom';
+import gtdLogo from '../logo-gtd-normal.png';
 export default class Home extends Component {
   // constructor(props) {
   //   super(props);
@@ -26,6 +27,17 @@ export default class Home extends Component {
             </div>
             <div className="col-sm-2_5">
               <Link to="/contact">Contact Us</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="row landing-banner">
+          <div className="col-sm-12">
+            <div className="row">
+              <img src={gtdLogo} className="landing-gtd"/>
+            </div>
+            <div className="row coming-soon">
+              COMING SOON
             </div>
           </div>
         </div>
