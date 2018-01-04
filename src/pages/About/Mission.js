af3f1659aff0be44c4a3961fcd7979851bde05cf
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-01-05 00:19:37 +0700

    about us styling

--- src/pages/About/Mission.js
+++ src/pages/About/Mission.js
@@ -7,27 +7,27 @@ export class Mission extends React.Component {
                 <div className="col-md-12">
                     <div className="about-box">
                         <h1 className="title">Misi Kami</h1>
-                        <div className="mission-container">
-                            <div className="col-md-3 col-xs-12">
+                        <div className="mission-container clearfix">
+                            <div className="col-md-3 col-xs-12 mission-item">
                                 <img src="/images/ball.svg" alt="ball" className="img-responsive logo"/>
                                 <p>
                                     Menjadikan GTD yang menyenangkan dan kokoh dan terorganisir
                                 </p>
                             </div>
-                            <div className="col-md-3 col-xs-12">
+                            <div className="col-md-3 col-xs-12 mission-item">
                                 <img src="/images/relationship.svg" alt="ball" className="img-responsive logo"/>
                                 <p>
                                     Mempersatukan hubungan antara freshie dan freshie, senior dan senior, freshie dan senior melalui GTD.
 
                                 </p>
                             </div>
-                            <div className="col-md-3 col-xs-12">
+                            <div className="col-md-3 col-xs-12 mission-item">
                                 <img src="/images/improve.png" alt="ball" className="img-responsive logo"/>
                                 <p>
                                     Menjadikan sarana pengembangan diri bagi seluruh komite dan peserta GTD
                                 </p>
                             </div>
-                            <div className="col-md-3 col-xs-12">
+                            <div className="col-md-3 col-xs-12 mission-item">
                                 <img src="/images/merlion.svg" alt="ball" className="img-responsive logo"/>
                                 <p>
                                     Mengenalkan freshie kepada kehidupan dan budaya di Singapura
--- src/pages/About/Vision.js
+++ src/pages/About/Vision.js
@@ -3,12 +3,15 @@ import React from "react";
 export class Vision extends React.Component {
     render(){
         return(
-            <div className="row vision-bg full-height-screen flex-center">
-                <div className="col-md-12">
-                    <h1 className="title">Visi Kami</h1>
-                    <p className="vision">
-                        "Sebagai gerbang penyambutan pelajar Indonesia baru di NTU"
-                    </p>
+            <div className="row flex-center">
+                <div className="col-md-12 vision-container">
+                    <img src="/images/vision.jpg" alt="vision" className="img-responsive" />
+                    <div className="vision-box">
+                        <h1 className="title">Visi Kami</h1>
+                        <p className="vision">
+                            "Sebagai gerbang penyambutan pelajar Indonesia baru di NTU"
+                        </p>
+                    </div>
                 </div>
             </div>
         )
