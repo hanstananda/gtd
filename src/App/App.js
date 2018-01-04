dfdf93e86c5dfa9c5597ea22ae830477100e9338
jasonlimantoro <jasonlimantoro99@gmail.com>
2018-01-04 08:30:20 +0700

    conditional rendering based on window.innerWidth

--- src/App/App.js
+++ src/App/App.js
@@ -13,24 +13,67 @@ import About from '../pages/About/About';
 import History from '../pages/History/History';
 import Anthem from '../pages/Anthem/Anthem';
 import Contact from '../pages/Contact/Contact';
-import { TopMenu } from '../components/Menu';
+import { Sidebar } from '../components/Sidebar';
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			isDesktop : false
+		};
+		this.updateView = this.updateView.bind(this);
+	};
+
+	componentDidMount(){
+		this.updateView();
+		window.addEventListener('resize', this.updateView);
+	}
+
+	componentWillUnmount(){
+		this.updateView();
+		window.removeEventListener('resize', this.updateView);
+	}
+
+	updateView(){
+		this.setState({
+			isDesktop : window.innerWidth > 1200
+		});
+	}
+
+
 	render() {
-		return (
-			<div id="outer-container" className="App">
-				<TopMenu />
-				<Router>
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route path="/about" component={About} />
-						<Route path="/history" component={History} />
-						<Route path="/anthem" component={Anthem} />
-						<Route path="/contact" component={Contact} />
-					</Switch>
-				</Router>
-			</div>
-		);
+		const isDesktop = this.state.isDesktop;
+		if (isDesktop) {
+			return (
+				<div className="App">
+					<Router>
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route path="/about" component={About} />
+							<Route path="/history" component={History} />
+							<Route path="/anthem" component={Anthem} />
+							<Route path="/contact" component={Contact} />
+						</Switch>
+					</Router>
+				</div>
+			)
+		}
+		else {
+			return (
+				<div id="outer-container" className="App">
+					<Sidebar />
+					<Router>
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route path="/about" component={About} />
+							<Route path="/history" component={History} />
+							<Route path="/anthem" component={Anthem} />
+							<Route path="/contact" component={Contact} />
+						</Switch>
+					</Router>
+				</div>
+			);
+		}
 	}
 }
 
